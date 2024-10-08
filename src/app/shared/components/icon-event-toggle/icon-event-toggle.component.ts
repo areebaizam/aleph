de24ee05684a95fa03b8a0b488627067552f4bd0
Areeb <areebaizam@gmail.com>
2024-10-08 13:13:55 -0700

    Configure colors

--- src/app/shared/components/icon-event-toggle/icon-event-toggle.component.ts
+++ src/app/shared/components/icon-event-toggle/icon-event-toggle.component.ts
@@ -1,10 +1,11 @@
 import { Component, computed, inject, input } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
 import { ToggleService } from '@core/services/toggle.service';
 import { EventToggleModel } from '@shared/models';
 
-const materialModules = [MatButtonModule, MatIconModule]
+const materialModules = [MatButtonModule, MatIconModule, MatTooltipModule]
 
 @Component({
   selector: 'tap-icon-event-toggle',
